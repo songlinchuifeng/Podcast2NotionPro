e11ddb44b9982d5dd9f6e38dd30225d5cba61493
malinkang <malinkang@adsconflux.com>
2024-08-08 17:16:10 +0800

    自动转写

--- scripts/speech_text.py
+++ scripts/speech_text.py
@@ -1,6 +1,9 @@
 import argparse
 import json
 import os
+import re
+import time
+import emoji
 import pendulum
 from retrying import retry
 import requests
@@ -37,24 +40,21 @@ headers = {
 @retry(stop_max_attempt_number=3, wait_fixed=5000)
 def get_dir():
     """获取文件夹"""
-    results = []
     url = (
         "https://qianwen.biz.aliyun.com/assistant/api/record/dir/list/get?c=tongyi-web"
     )
     response = requests.post(url, headers=headers)
-    if response.status_code == 200:
+    if response.ok:
         r = response.json()
         success = r.get("success")
         errorMsg = r.get("errorMsg")
         if success:
-            data = r.get("data")
-            for i in data:
-                results.extend(dir_list(i.get("dir").get("id")))
+            return r.get("data")
+
         else:
             print(f"请求失败：{errorMsg}")
     else:
         print("请求失败：", response.status_code)
-    return results
 
 
 @retry(stop_max_attempt_number=3, wait_fixed=5000)
@@ -64,7 +64,12 @@ def dir_list(dir_id):
     pageNo = 1
     pageSize = 48
     while True:
-        payload = {"dirIdStr": dir_id, "pageNo": pageNo, "pageSize": pageSize}
+        payload = {
+            "dirIdStr": dir_id,
+            "pageNo": pageNo,
+            "pageSize": pageSize,
+            "status": [20, 30, 40, 41],
+        }
         url = "https://qianwen.biz.aliyun.com/assistant/api/record/list?c=tongyi-web"
         response = requests.post(url, headers=headers, json=payload)
         if response.status_code == 200:
@@ -89,7 +94,7 @@ def get_note(transId):
     if response.ok:
         note = response.json().get("data").get("content")
         if note:
-            data = json.loads(content)
+            data = json.loads(note)
             children = []
             for paragraph in data:
                 type = "paragraph"
@@ -191,25 +196,30 @@ def get_all_lab_info(transId):
                 for contentValue in content.get("contentValues"):
                     if name == "全文摘要":
                         value = contentValue.get("value")
-                        children.insert(5,utils.get_heading(3, "全文摘要"))
-                        children.insert(6,utils.get_callout(value, {"emoji": "💡"}))
+                        children.append(utils.get_heading(3, "全文摘要"))
+                        children.append(utils.get_callout(value, {"emoji": "💡"}))
                     if name == "思维导图":
                         mindmap = contentValue.get("json")
                     if name == "议程":
                         title = f"{utils.format_milliseconds(contentValue.get('time'))} {contentValue.get('value')}"
                         children.append(utils.get_heading(3, title))
                         summary = contentValue.get("summary")
-                        children.append(utils.get_callout(summary, {"emoji": "💡"}))
-                    if name == "qa问答":
+                        if summary:
+                            children.append(utils.get_callout(summary, {"emoji": "💡"}))
+                    if name == "qa问答" and contentValue.get("extensions"):
                         title = contentValue.get("title")
                         value = contentValue.get("value")
-                        beginTime = (
-                            contentValue.get("extensions")[0]
-                            .get("sentenceInfoOfAnswer")[0]
-                            .get("beginTime")
-                        )
-                        beginTime = utils.format_milliseconds(beginTime)
-                        title = f"{beginTime} {title}"
+                        if contentValue.get("extensions")[0].get(
+                            "sentenceInfoOfAnswer"
+                        ):
+                            beginTime = (
+                                contentValue.get("extensions")[0]
+                                .get("sentenceInfoOfAnswer")[0]
+                                .get("beginTime")
+                            )
+                            if beginTime:
+                                beginTime = utils.format_milliseconds(beginTime)
+                                title = f"{beginTime} {title}"
                         children.append(utils.get_heading(3, title))
                         children.append(utils.get_callout(value, {"emoji": "💡"}))
         return (children, mindmap)
@@ -217,19 +227,40 @@ def get_all_lab_info(transId):
         print("请求脑图失败：", response.status_code)
 
 
-def insert_mindmap(block_id, mindmap):
-    """插入思维导图"""
-    id = (
-        notion_helper.append_blocks(
-            block_id=block_id,
-            children=[utils.get_bulleted_list_item(mindmap.get("content"))],
+def insert_mindmap(mindmap):
+    mindmap_page_id = None
+    if mindmap:
+        mindmap_page_id = create_mindmap(title, episode.get("icon"))
+        start_time = time.time()
+        print(f"开始插入思维导图")
+        mindmap_root_id = (
+            notion_helper.append_blocks(
+                block_id=mindmap_page_id,
+                children=[utils.get_bulleted_list_item(mindmap.get("content"))],
+            )
+            .get("results")[0]
+            .get("id")
         )
-        .get("results")[0]
-        .get("id")
-    )
-    if mindmap.get("children"):
-        for child in mindmap.get("children"):
-            insert_mindmap(id, child)
+        insert_mindmap_to_notion(mindmap_root_id, mindmap.get("children"))
+        update_mindmap(mindmap_page_id)
+        end = time.time()
+        print(f"插入思维导图结束 {end-start_time}")
+        mindmap_url = f"https://mindmap.malinkang.com/markmap/{mindmap_page_id.replace('-','')}?token={os.getenv('NOTION_TOKEN')}"
+        children.append(utils.get_heading(2, "思维导图"))
+        children.append({"type": "embed", "embed": {"url": mindmap_url}})
+    return mindmap_page_id
+
+
+def insert_mindmap_to_notion(block_id, children):
+    """将思维导图插入Notion中"""
+    blocks = [utils.get_bulleted_list_item(block.get("content")) for block in children]
+    results = notion_helper.append_blocks(
+        block_id=block_id,
+        children=blocks,
+    ).get("results")
+    for index, child in enumerate(children):
+        if child.get("children"):
+            insert_mindmap_to_notion(results[index].get("id"), child.get("children"))
 
 
 def check_mindmap(title):
@@ -255,7 +286,7 @@ def create_mindmap(title, icon):
         "type": "database_id",
     }
     properties = {
-        "标题":{"title": [{"type": "text", "text": {"content": title}}]},
+        "标题": {"title": [{"type": "text", "text": {"content": title}}]},
         "状态": {"status": {"name": "In progress"}},
     }
 
@@ -265,6 +296,7 @@ def create_mindmap(title, icon):
         icon=icon,
     ).get("id")
     return mindmap_page_id
+
 
 def update_mindmap(page_id):
     properties = {
@@ -286,11 +318,13 @@ def get_trans_result(transId):
     }
     url = "https://tw-efficiency.biz.aliyun.com/api/trans/getTransResult?c=tongyi-web"
     response = requests.post(url, headers=headers, json=payload)
-    if response.status_code == 200:
+    if response.ok:
         response_data = response.json()
         user_dict = {}
         if response_data.get("data").get("tag").get("identify"):
-            user_info = json.loads(response_data.get("data").get("tag").get("identify")).get("user_info")
+            user_info = json.loads(
+                response_data.get("data").get("tag").get("identify")
+            ).get("user_info")
             for key, value in user_info.items():
                 user_dict[key] = value.get("name")
         children = []
@@ -320,6 +354,7 @@ def get_trans_result(transId):
 author_cache = {}
 
 
+@retry(stop_max_attempt_number=3, wait_fixed=5000)
 def get_author_avatar(author):
     if author in author_cache:
         return author_cache[author]
@@ -331,70 +366,223 @@ def get_author_avatar(author):
         return avatar
 
 
+@retry(stop_max_attempt_number=3, wait_fixed=5000)
+def create_dir(name):
+    """创建文件夹，支持创建重名文件夹"""
+    payload = {"dirName": name, "parentIdStr": -1}
+    url = "https://qianwen.biz.aliyun.com/assistant/api/record/dir/add?c=tongyi-web"
+    r = requests.post(url, headers=headers, json=payload)
+    if r.ok:
+        return r.json().get("data").get("focusDir").get("idStr")
+
+
+@retry(stop_max_attempt_number=3, wait_fixed=5000)
+def parseNetSourceUrl(rss_url):
+    print("start parse url")
+    payload = {"action": "parseNetSourceUrl", "version": "1.0", "url": rss_url}
+    url = (
+        "https://tw-efficiency.biz.aliyun.com/api/trans/parseNetSourceUrl?c=tongyi-web"
+    )
+    r = requests.post(url, headers=headers, json=payload)
+    if r.ok:
+        data = r.json()
+        success = data.get("success")
+        print(f"parse url success {success}")
+        if success:
+            return data.get("data").get("taskId")
+
+
+@retry(stop_max_attempt_number=3, wait_fixed=5000)
+def start(dir_id, files):
+    payload = {
+        "dirIdStr": dir_id,
+        "files": files,
+        "taskType": "net_source",
+        "bizTerminal": "web",
+    }
+    url = "https://qianwen.biz.aliyun.com/assistant/api/record/blog/start?c=tongyi-web"
+    response = requests.post(url, headers=headers, json=payload)
+
+
+@retry(stop_max_attempt_number=3, wait_fixed=5000)
+def queryNetSourceParse(task_id, dir_id, trans_list):
+    payload = {"action": "queryNetSourceParse", "version": "1.0", "taskId": task_id}
+    url = "https://tw-efficiency.biz.aliyun.com/api/trans/queryNetSourceParse?c=tongyi-web"
+    response = requests.post(url, headers=headers, json=payload)
+    results = []
+    if response.ok:
+        data = response.json().get("data")
+        status = data.get("status")
+        print(f"query source status {status}")
+        if status == 0:
+            urls = data.get("urls")
+            for url in urls:
+                if url.get("showName") in trans_list:
+                    print(f"将《{url.get('showName')}》添加到转写列表中")
+                    results.append(
+                        {
+                            "fileId": url.get("fileId"),
+                            "dirId": dir_id,
+                            "fileSize": url.get("size"),
+                            "tag": {
+                                "fileType": "net_source",
+                                "showName": url.get("showName"),
+                                "lang": "cn",
+                                "roleSplitNum": 0,
+                                "translateSwitch": 0,
+                                "transTargetValue": 0,
+                                "client": "web",
+                                "originalTag": "",
+                            },
+                        }
+                    )
+            return results
+        elif status == -1:
+            time.sleep(1)
+            return queryNetSourceParse(task_id=task_id, dir_id=dir_id,trans_list=trans_list)
+        else:
+            print(f"query source data = {data}")
+            return None
+
+
+@retry(stop_max_attempt_number=3, wait_fixed=5000)
+def start_trans(dir_name, rss, trans_list):
+    print(f"开始转写{dir_name}")
+    dir = list(filter(lambda x: x.get("dir").get("dirName") == dir_name, all_dirs))
+    if len(dir) > 0:
+        dir_str_id = dir[0].get("dir").get("idStr")
+    else:
+        dir_str_id = create_dir(dir_name)
+    task_id = parseNetSourceUrl(rss)
+    files = queryNetSourceParse(
+        task_id=task_id, dir_id=dir_str_id, trans_list=trans_list
+    )
+    if files:
+        for i in range(0, len(files) // 50 + 1):
+            start(dir_id=dir_str_id, files=files[i * 50 : (i + 1) * 50])
+
+
+cache = {}
+
+
+def get_podcast(ids):
+    podcast_page_id = ids[0].get("id")
+    if id not in cache:
+        podcast_properties = notion_helper.client.pages.retrieve(podcast_page_id).get(
+            "properties"
+        )
+        cache[podcast_page_id] = podcast_properties
+    return cache.get(podcast_page_id)
+
+
+def get_dir_id_by_name(dir_name):
+    dir = list(filter(lambda x: x.get("dir").get("dirName") == dir_name, all_dirs))
+    if dir:
+        return dir[0].get("dir").get("id")
+
+
+def handle_title(title):
+    """处理标题，通义转写的时候有的emoji表情会被去掉"""
+    no_emoji_title = emoji.replace_emoji(title, replace="")
+    return re.sub(r"\s+", " ", no_emoji_title).strip()
+
+
 if __name__ == "__main__":
     notion_helper = NotionHelper()
     headers["cookie"] = os.getenv("COOKIE").strip()
-    filter = {
+    f = {
         "and": [
             {"property": "语音转文字状态", "status": {"does_not_equal": "Done"}},
-            {"property": "语音转文字", "checkbox": {"equals": True}},
+            {"property": "Podcast", "relation": {"is_not_empty": True}},
         ]
     }
     episodes = notion_helper.query_all_by_filter(
-        notion_helper.episode_database_id, filter=filter
+        notion_helper.episode_database_id, filter=f
     )
-    results = get_dir()
-    results = {
-        x.get("recordTitle"):x.get("genRecordId")
-        for x in results
-    }
+    podcasts = {}
     for episode in episodes:
         episode_properties = episode.get("properties")
-        title = utils.get_property_value(episode_properties.get("标题"))
-        children = []
-        if title in results:
-            episode_page_id = episode.get("id")
-            audio_url = utils.get_property_value(episode_properties.get("音频"))
-            podcast = utils.get_property_value(episode_properties.get("Podcast"))
-            podcast = utils.get_property_value(
-                notion_helper.client.pages.retrieve(podcast[0].get("id"))
-                .get("properties")
-                .get("播客")
-            )
-            transId = results.get(title)
-            cover = episode.get("cover").get("external").get("url")
-            children.append(utils.get_heading(2, "音频"))
-            player_url = f"https://notion-music.malinkang.com/player?url={urllib.parse.quote(audio_url)}&name={urllib.parse.quote(title)}&cover={urllib.parse.quote(cover)}&artist={urllib.parse.quote(podcast)}"
-            children.append({"type": "embed", "embed": {"url": player_url}})
-            info, mindmap = get_all_lab_info(transId)
-            mindmap_page_id = None
-            if mindmap:
-                mindmap_page_id = create_mindmap(title, episode.get("icon"))
-                insert_mindmap(mindmap_page_id, mindmap)
-                update_mindmap(mindmap_page_id)
-                mindmap_url = f"https://mindmap.malinkang.com/markmap/{mindmap_page_id.replace('-','')}?token={os.getenv('NOTION_TOKEN')}"
-                children.append(utils.get_heading(2, "思维导图"))
-                children.append({"type": "embed", "embed": {"url": mindmap_url}})
-            if info:
-                children.extend(info)
-            trans = get_trans_result(transId)
-            if trans:
-                children.append(utils.get_heading(2, "语音转文字"))
-                children.extend(trans)
-            note = get_note(transId)
-            if note:
-                children.append(utils.get_heading(2, "笔记"))
-                children.extend(note)
-            for i in range(0, len(children) // 100 + 1):
-                notion_helper.append_blocks(
-                    block_id=episode_page_id, children=children[i * 100 : (i + 1) * 100]
-                )
-            properties = {"语音转文字状态": {"status": {"name": "Done"}}}
-            if mindmap_page_id:
-                properties["思维导图"] = {"relation": [{"id": mindmap_page_id}]}
-            notion_helper.update_page(
-                page_id=episode_page_id,
-                properties=properties,
-            )
+        podcast = utils.get_property_value(episode_properties.get("Podcast"))
+        podcast_properties = get_podcast(podcast)
+        podcast_title = utils.get_property_value(podcast_properties.get("播客"))
+        rss = utils.get_property_value(podcast_properties.get("rss"))
+        if podcast_title not in podcasts:
+            podcasts[podcast_title] = {"rss": rss, "episodes": []}
+        podcasts[podcast_title].get("episodes").append(episode)
+    print(f"获取播客成功：{len(episodes)}")
+    all_dirs = get_dir()
+    for key, value in podcasts.items():
+        dir_id = get_dir_id_by_name(key)
+        need_trans = False
+        trans_list = []
+        if dir_id:
+            print(f"{key} 存在")
+            records = dir_list(dir_id)
+            records = {
+                x.get("recordTitle"): x for x in records if x.get("recordStatus") != 40
+            }
+            print(f"获取转写成功：{len(records)}")
+            for episode in value.get("episodes"):
+                episode_properties = episode.get("properties")
+                title = utils.get_property_value(episode_properties.get("标题"))
+                children = []
+                # 20 正在转 30是成功 40是失败
+                if title not in records:
+                    title = handle_title(title)
+                if title in records:
+                    if records.get(title).get("recordStatus") != 30:
+                        continue
+                    episode_page_id = episode.get("id")
+                    audio_url = utils.get_property_value(episode_properties.get("音频"))
+                    transId = records.get(title).get("genRecordId")
+                    cover = episode.get("cover").get("external").get("url")
+                    children.append(utils.get_heading(2, "音频"))
+                    if audio_url and title and cover and podcast_title:
+                        player_url = f"https://notion-music.malinkang.com/player?url={urllib.parse.quote(audio_url)}&name={urllib.parse.quote(title)}&cover={urllib.parse.quote(cover)}&artist={urllib.parse.quote(podcast_title)}"
+                        children.append({"type": "embed", "embed": {"url": player_url}})
+                    print(f"开始获取《{title}》的数据")
+                    info, mindmap = get_all_lab_info(transId)
+                    mindmap_page_id = insert_mindmap(mindmap)
+                    if info:
+                        children.extend(info)
+                    trans = get_trans_result(transId)
+                    if trans:
+                        children.append(utils.get_heading(2, "语音转文字"))
+                        children.extend(trans)
+                    note = get_note(transId)
+                    if note:
+                        children.append(utils.get_heading(2, "笔记"))
+                        children.extend(note)
+                    start_time = time.time()
+                    print(f"开始插入其他数据")
+                    for i in range(0, len(children) // 100 + 1):
+                        notion_helper.append_blocks(
+                            block_id=episode_page_id,
+                            children=children[i * 100 : (i + 1) * 100],
+                        )
+                    end = time.time()
+                    print(f"插入其他数据结束 {end-start_time}")
+                    properties = {"语音转文字状态": {"status": {"name": "Done"}}}
+                    if mindmap_page_id:
+                        properties["思维导图"] = {"relation": [{"id": mindmap_page_id}]}
+                    notion_helper.update_page(
+                        page_id=episode_page_id,
+                        properties=properties,
+                    )
+                else:
+                    print(f"未找到《{title}》")
+                    need_trans = True
+                    trans_list.append(title)
+                    trans_list.append(handle_title(title))
         else:
-            print(f"未搜索到《{title}》，请检查你是否已经转写成功")
+            trans_list = [
+                utils.get_property_value(x.get("properties").get("标题"))
+                for x in value.get("episodes")
+            ]
+            trans_list.extend([handle_title(x) for x in trans_list])
+            need_trans = True
+        if need_trans:
+            if value.get("rss"):
+                start_trans(key, value.get("rss"), trans_list)
+            else:
+                print(f"{key}的rss为空")
